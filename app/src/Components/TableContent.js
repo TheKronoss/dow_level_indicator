08774939a73a670fa047a0471b398f3e943a3118
wongus <wongus.in@hotmail.com>
2021-04-16 10:44:02 +0200

    add logic

--- app/src/Components/TableContent.js
+++ app/src/Components/TableContent.js
@@ -17,12 +17,18 @@ export default function TableContent() {
     let _new = parseSearch[2].pop();
     let _old = parseSearch[2].shift();
 
+    let _new2 = parsed[currentCabinet]['bottle2'][2].pop();
+    let _old2 = parsed[currentCabinet]['bottle2'][2].shift();
+
     let colour = null
     let innerText = ''
 
+    let colour2 = null
+    let innerText2 = ''
+
     // sets table labels
     function applyLabels() {
-        return ((_new - _old) / _old) * 100 > 10 ? applyGreen() : applyRed();
+        return (_new / _old * 100 > 10) ? applyGreen() : applyRed();
     }
 
     let applyGreen = () => { colour = 'px-2 inline-flex text-xs leading-5 font-semibold bg-green-100 text-green-800 rounded-full'; innerText = 'clear'; }
@@ -30,16 +36,38 @@ export default function TableContent() {
 
     applyLabels();
 
+    function applyLabels2() {
+        return (_new2 / _old2) * 100 > 10 ? applyGreen2() : applyRed2();
+    }
+
+    let applyGreen2 = () => { colour2 = 'px-2 inline-flex text-xs leading-5 font-semibold bg-green-100 text-green-800 rounded-full'; innerText2 = 'clear'; }
+    let applyRed2 = () => { colour2 = 'px-2 inline-flex text-xs leading-5 font-semibold bg-red-100 text-red-800 rounded-full'; innerText2 = 'refill'; }
+
+    applyLabels2();
+
     return (
-        <tr>
-            <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{parseSearch[0]}</td>
-            <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{parseSearch[1]}</td>
-            <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{_new} / {_old} kg</td>
-            <td id='status' class="px-6 text-left py-4 whitespace-nowrap text-gray-500">
-                <span class={colour}>
-                    {innerText}
-                </span>
-            </td>
-        </tr>
+        <tbody class="bg-white divide-y divide-gray-200">
+            <tr>
+                <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{parsed[currentCabinet]['bottle1'][0]}</td>
+                <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{parsed[currentCabinet]['bottle1'][1]}</td>
+                <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{_new} / {_old} kg</td>
+                <td id='status' class="px-6 text-left py-4 whitespace-nowrap text-gray-500">
+                    <span class={colour}>
+                        {innerText}
+                    </span>
+                </td>
+            </tr>
+            <tr>
+                <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{parsed[currentCabinet]['bottle2'][0]}</td>
+                <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{parsed[currentCabinet]['bottle2'][1]}</td>
+                <td class="px-6 text-left py-4 whitespace-nowrap text-sm text-gray-500">{_new2} / {_old2} kg</td>
+                <td id='status' class="px-6 text-left py-4 whitespace-nowrap text-gray-500">
+                    <span class={colour2}>
+                        {innerText2}
+                    </span>
+                </td>
+            </tr>
+        </tbody>
+
     )
 }
--- app/src/localstorage.js
+++ app/src/localstorage.js
@@ -11,7 +11,7 @@ let data = {
     "cabinet2": {
         "bottle1": ['01/01/2021', '1# fluoride', [80, 20, 19, 10]
         ],
-        "bottle2": ['01/01/2021', '2# carbon-dioxide', [200, 30, 20, 10]
+        "bottle2": ['01/01/2021', '2# carbon-dioxide', [40, 30, 20, 2]
         ]
     },
 
